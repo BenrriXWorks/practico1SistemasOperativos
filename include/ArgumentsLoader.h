cb6fd24f098a4b5bb8b801516c7bda2c4a107e1e
benrriXWorks <BenrriXWorks@gmail.com>
2023-09-21 20:57:18 -0300

    Modificado ArgumentsLoader

--- include/ArgumentsLoader.h
+++ include/ArgumentsLoader.h
@@ -7,7 +7,7 @@
 class ArgumentsLoader {
 private:
     static bool loaded;
-    static std::string i,o,u,f,t,v;
+    static std::string u,p,v;
     static std::vector<int> v_vector;
     static ArgumentsLoader* instance;
     ArgumentsLoader(){};
@@ -15,11 +15,8 @@ public:
     static ArgumentsLoader* getInstance();
     static bool init(int argc,char** argv);
 
-    inline std::string get_i(){return i;}
-    inline std::string get_o(){return o;}
+    inline std::string get_p(){return p;}
     inline std::string get_u(){return u;}
-    inline std::string get_f(){return f;}
-    inline std::string get_t(){return t;}
     inline std::string get_v(){return v;}
     inline std::vector<int> get_v_vector(){return v_vector;}
 };
--- src/ArgumentsLoader/ArgumentsLoader.cpp
+++ src/ArgumentsLoader/ArgumentsLoader.cpp
@@ -2,12 +2,9 @@
 using namespace std;
 
 std::string 
-    ArgumentsLoader::i="",
-    ArgumentsLoader::o="",
+    ArgumentsLoader::p="",
     ArgumentsLoader::v="",
-    ArgumentsLoader::u="",
-    ArgumentsLoader::f="",
-    ArgumentsLoader::t="";
+    ArgumentsLoader::u="";
 std::vector<int> ArgumentsLoader::v_vector = vector<int>();
 ArgumentsLoader* ArgumentsLoader::instance = nullptr;
 bool ArgumentsLoader::loaded = false;
@@ -23,14 +20,11 @@ bool ArgumentsLoader::init(int argc,char** argv){
         return (printf("ArgumentsLoader: Esta clase ya se ha inicializado\n"),true);
 
     int opt;
-    char* i_aux=nullptr, *o_aux=nullptr, *v_aux=nullptr, *u_aux=nullptr, *f_aux=nullptr, *t_aux=nullptr;
-    while ((opt = getopt(argc, argv, "i:o:v:u:f:t:")) != -1) {
+    char* p_aux=nullptr, *v_aux=nullptr, *u_aux=nullptr;
+    while ((opt = getopt(argc, argv, "u:p:v:")) != -1) {
         switch (opt) {
-            case 'i':
-                i_aux = optarg;
-                break;
-            case 'o':
-                o_aux = optarg;
+            case 'p':
+                p_aux = optarg;
                 break;
             case 'v':
                 v_aux = optarg;
@@ -38,30 +32,19 @@ bool ArgumentsLoader::init(int argc,char** argv){
             case 'u':
                 u_aux = optarg;
                 break;
-            case 'f':
-                f_aux = optarg;
-                break;
-            case 't':
-                t_aux = optarg;
-                break;
             default:
                 printf("ArgumentsLoader: Argumento invalido o vacio \"-%c\".\n", optopt);
                 return false;
         }
     }
+
     bool correct=true;
     if (u_aux == nullptr || (u = strip(string(u_aux))).empty())
         printf("ArgumentsLoader: Debe ingresar un usuario con -u {user}\n"),correct=false;
-    if (i_aux != nullptr && (i = strip(string(i_aux))).empty())
-        printf("ArgumentsLoader: El argumento -i esta vacio\n"),correct=false;
-    if (o_aux != nullptr && (o = strip(string(o_aux))).empty())
-        printf("ArgumentsLoader: El argumento -o esta vacio\n"),correct=false;
     if (v_aux != nullptr && (v = stripAllOf(string(v_aux)," ;")).empty())
         printf("ArgumentsLoader: El argumento -v esta vacio\n"),correct=false;
-    if (f_aux != nullptr && (f = strip(string(f_aux))).empty())
-        printf("ArgumentsLoader: El argumento -f esta vacio\n"),correct=false;
-    if (t_aux != nullptr && (t = strip(string(t_aux))).empty())
-        printf("ArgumentsLoader: El argumento -t esta vacio\n"),correct=false;
+    if (p_aux != nullptr && (p = strip(string(p_aux))).empty())
+        printf("ArgumentsLoader: El argumento -p esta vacio\n"),correct=false;
     if (!correct)
         return false;
     
