b8f639034f3dfef526df985f5d1da8a66dc77959
benrriXWorks <BenrriXWorks@gmail.com>
2023-09-21 22:25:01 -0300

    Ahora se llaman ags los argumentos en main

--- main.cpp
+++ main.cpp
@@ -10,18 +10,18 @@ int main(int argc, char** argv){
     EnvLoader* envLoader = EnvLoader::getInstance();
     Menu* menu = Menu::getInstance();
     UserDB* userDB = UserDB::getInstance();
-    ArgumentsLoader* argumentsLoader = ArgumentsLoader::getInstance();
+    ArgumentsLoader* args = ArgumentsLoader::getInstance();
 
-    if (!argumentsLoader->init(argc,argv))
+    if (!args->init(argc,argv))
         return(printf("Main: No se inicializaron los argumentos correctamente\n"),EXIT_FAILURE);
     if (!envLoader->init())
         return (printf("Main: No se cargaron las variables de entorno correctamente\n"),EXIT_FAILURE);
     if (!userDB->loadUsers(envLoader->getUserDB_path()))
         return (printf("Main: No se cargaron los usuarios correctamente\n"),EXIT_FAILURE);
-    if (!userDB->fetchUser(argumentsLoader->get_u()))
-        return (printf("Main: El usuario {%s} no existe\n",argumentsLoader->get_u().c_str()),EXIT_FAILURE);
+    if (!userDB->fetchUser(args->get_u()))
+        return (printf("Main: El usuario {%s} no existe\n",args->get_u().c_str()),EXIT_FAILURE);
 
-    User* user = userDB->login(argumentsLoader->get_u(), argumentsLoader->get_p());
+    User* user = userDB->login(args->get_u(), args->get_p());
     if (user == nullptr)
         return (printf("Main: Constrasena invalida\n"),EXIT_FAILURE);
     
