92adbfb273010281df6ff54758caf6e402bfc921
benrriXWorks <BenrriXWorks@gmail.com>
2023-09-21 22:30:03 -0300

    La funcion 0 ahora deberia limpiar la pantalla tanto en linux como windows

--- src/Funciones/ConcreteFunctionWrapper_0.cpp
+++ src/Funciones/ConcreteFunctionWrapper_0.cpp
@@ -1,7 +1,11 @@
 #include "../../include/ConcreteFunctionWrapper_0.h"
 
 bool ConcreteFunctionWrapper_0 :: execute(){
-    system("clear");
+    #ifdef _WIN32
+        system("cls");
+    #else
+        system("clear");
+    #endif
     printf("Saliendo!\n");
     exit(EXIT_SUCCESS);
 }
