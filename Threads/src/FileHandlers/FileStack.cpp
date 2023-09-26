80e3fd879b5e30af012cf89c7c74a47913a75f22
benrriXWorks <BenrriXWorks@gmail.com>
2023-09-25 22:05:02 -0300

    #FileStack

--- Threads/src/FileHandlers/FileStack.cpp
+++ Threads/src/FileHandlers/FileStack.cpp
@@ -1,6 +1,10 @@
 #include "../../include/FileStack.h"
 
 FileReader& FileStack::next(){
+
+    if (cola.empty())
+        throw std::runtime_error("La cola esta vacia");
+
     FileReader *file = cola.back();
     cola.pop_back();
     return (*file);
@@ -10,10 +14,12 @@ FileReader& FileStack::next(){
 FileStack::FileStack(std::vector<std::string> listOfPaths){
 
     for (std::string s : listOfPaths){
-        FileReader *fr = &FileReader();
-        if (!fr->open(s))
+        FileReader *fr = new FileReader();
+        if (!fr->open(s)){
             printf("El archivo en {%s} no ha podido ser leido. Saltando.\n",s);
+            delete fr;
+        }
         cola.push_back(fr);        
     }
-    
+
 };
